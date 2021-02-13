211a85b16dec1b6a47a8554d63dbbd80c9042fc7
Yan Kin Luong <yan.kin.luong@me.com>
2021-02-13 00:47:17 -0600

    test new random code

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+#print("hello world!")
+# Define sharks variable as a list of strings
+sharks = ['hammerhead', 'great white', 'dogfish', 'frilled', 'bullhead', 'requiem']
+
+# For loop that iterates over sharks list and prints each string item
+for shark in sharks:
+   print(shark)
